ef5098d9f61e0aa035f0edc64979b5adf1f80f9d
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-17 18:42:40 -0300

    chore: Add stylistic rules to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
+const stylistic = require("@stylistic/eslint-plugin")
+
+const { convertToWarn } = require("./lib/convertToWarn")
+
+
+const recommendedExtendsRules = convertToWarn(
+    stylistic.configs["recommended-extends"].rules
+)
+
 
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
@@ -16,152 +25,117 @@ module.exports = {
         node: true,
     },
     rules: {
-        "array-bracket-spacing": [
-            "warn",
-            "never",
-            {
-                "singleValue": false,
-                "objectsInArrays": true,
-                "arraysInArrays": true
-            }
-        ],
-        "arrow-body-style": [
-            "warn",
-            "as-needed"
-        ],
-        "array-element-newline": [
-            "warn",
-            "consistent"
-        ],
-        "arrow-parens": [
-            "warn",
-            "as-needed"
-        ],
-        "arrow-spacing": [
-            "warn",
-            {
-                "before": true,
-                "after": true
-            }
-        ],
-        "brace-style": [
-            "warn",
-            "1tbs",
-            {
-                "allowSingleLine": true
-            }
-        ],
-        "camelcase": "warn",
-        "comma-dangle": [
-            "warn",
-            "only-multiline"
-        ],
-        "comma-style": "off",
-        "eol-last": [
-            "warn",
-            "always"
-        ],
-        "function-call-argument-newline": [
-            "warn",
-            "consistent"
-        ],
-        "indent": [
-            "warn",
-            2,
-            {
-                "SwitchCase": 1,
-                "ignoredNodes": [
-                    "FunctionExpression > .params[decorators.length > 0]",
-                    "FunctionExpression > .params > :matches(Decorator, :not(:first-child))",
-                    "ClassBody.body > PropertyDefinition[decorators.length > 0] > .key"
-                ]
-            }
-        ],
-        "jsx-quotes": [
-            "warn",
-            "prefer-double"
-        ],
-        "key-spacing": [
-            "warn",
-            {
-                "beforeColon": false,
-                "afterColon": true
-            }
-        ],
-        "keyword-spacing": [
-            "warn",
-            {
-                "before": true,
-                "after": true
-            }
-        ],
-        "max-len": [
-            "warn",
-            {
-                "code": 80,
-                "comments": 80,
-                "ignorePattern": "^\\s*var\\s.+=\\s*require\\s*\\(",
-                "ignoreRegExpLiterals": true,
-                "ignoreStrings": true,
-                "ignoreTemplateLiterals": true,
-                "ignoreTrailingComments": true,
-                "ignoreUrls": true
-            }
-        ],
-        "multiline-comment-style": [
-            "warn",
-            "separate-lines"
-        ],
-        "multiline-ternary": [
-            "warn",
-            "always-multiline"
-        ],
-        "no-empty-function": "off",
-        "no-multi-spaces": "warn",
-        "no-trailing-spaces": "warn",
-        "object-curly-spacing": [
-            "warn",
-            "always"
-        ],
-        "quotes": [
-            "warn",
-            "double"
-        ],
-        "semi": [
-            "warn",
-            "never"
-        ],
-        "space-before-blocks": [
-            "warn",
-            "always"
-        ],
-        "space-before-function-paren": [
-            "warn",
-            {
-                "anonymous": "always",
-                "named": "never",
-                "asyncArrow": "always"
-            }
-        ],
-        "space-in-parens": "warn",
-        "space-infix-ops": [
-            "warn",
-            {
-                "int32Hint": false
-            }
-        ],
-        "switch-colon-spacing": [
-            "warn",
-            {
-                "before": false,
-                "after": true
-            }
-        ],
-
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-empty-interface": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-var-requires": "off",
+        ...recommendedExtendsRules,
+        "@stylistic/array-bracket-newline": ["warn", "consistent"],
+        "@stylistic/arrow-parens": ["warn", "as-needed"],
+        "@stylistic/brace-style": ["warn", "1tbs"],
+        "@stylistic/dot-location": ["warn", "property"],
+        "@stylistic/function-call-spacing": ["warn", "never"],
+        "@stylistic/function-paren-newline": ["warn", "multiline-arguments"],
+        "@stylistic/generator-star-spacing": ["warn", {
+            before: true,
+            after: false,
+        }],
+        "@stylistic/implicit-arrow-linebreak": ["warn", "beside"],
+        "@stylistic/indent": ["warn", 4, {
+            SwitchCase: 1,
+            ignoredNodes: [
+                "FunctionExpression > .params[decorators.length > 0]",
+                "FunctionExpression > .params > :matches(Decorator, :not(:first-child))",
+                "ClassBody.body > PropertyDefinition[decorators.length > 0] > .key",
+            ],
+        }],
+        "@stylistic/indent-binary-ops": ["warn", 4],
+        "@stylistic/jsx-curly-brace-presence": ["warn", {
+            props: "always",
+            children: "ignore",
+            propElementValues: "always",
+        }],
+        "@stylistic/jsx-indent": ["warn", 4, {
+            checkAttributes: true,
+            indentLogicalExpressions: true,
+        }],
+        "@stylistic/jsx-indent-props": ["warn", 4],
+        "@stylistic/jsx-quotes": ["warn", "prefer-double"],
+        "@stylistic/jsx-self-closing-comp": ["error", {
+            component: true,
+            html: true,
+        }],
+        "@stylistic/max-len": ["warn", {
+            code: 90,
+            comments: 90,
+            ignorePattern: "^\\s*var\\s.+=\\s*require\\s*\\(",
+            ignoreRegExpLiterals: true,
+            ignoreTemplateLiterals: true,
+            ignoreUrls: true,
+        }],
+        "@stylistic/member-delimiter-style": ["warn", {
+            singleline: {
+                delimiter: "comma",
+                requireLast: false,
+            },
+            multiline: {
+                delimiter: "none",
+                requireLast: false,
+            },
+            multilineDetection: "brackets",
+        }],
+        "@stylistic/new-parens": ["warn", "always"],
+        "@stylistic/newline-per-chained-call": ["warn", {
+            ignoreChainWithDepth: 2,
+        }],
+        "@stylistic/no-confusing-arrow": ["warn", {
+            allowParens: true,
+            onlyOneSimpleParam: true,
+        }],
+        "@stylistic/no-floating-decimal": "warn",
+        "@stylistic/no-mixed-operators": ["warn", {
+            groups: [
+                ["+", "-", "*", "/", "%", "**"],
+                ["&", "|", "^", "~", "<<", ">>", ">>>"],
+                ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
+                ["&&", "||"],
+                ["in", "instanceof"],
+            ],
+            allowSamePrecedence: true,
+        }],
+        "@stylistic/no-mixed-spaces-and-tabs": "warn",
+        "@stylistic/no-multiple-empty-lines": ["warn", {
+            max: 2,
+            maxBOF: 1,
+            maxEOF: 0,
+        }],
+        "@stylistic/operator-linebreak": ["warn", "before", {
+            overrides: {
+                "=": "after",
+            },
+        }],
+        "@stylistic/quotes": ["warn", "double", {
+            avoidEscape: true,
+            allowTemplateLiterals: true,
+        }],
+        "@stylistic/semi": ["warn", "never", {
+            beforeStatementContinuationChars: "never",
+        }],
+        "@stylistic/spaced-comment": ["warn", "always", {
+            block: {
+                balanced: true,
+                exceptions: [],
+                markers: [],
+            },
+            line: {
+                exceptions: [],
+                markers: [],
+            },
+        }],
+        "@stylistic/switch-colon-spacing": ["warn", {
+            before: false,
+            after: true,
+        }],
+        "@stylistic/wrap-iife": ["warn", "inside", {
+            functionPrototypeMethods: true,
+        }],
+        "@stylistic/wrap-regex": "off",
+        "@stylistic/yield-star-spacing": ["warn", "before"],
     },
 }
