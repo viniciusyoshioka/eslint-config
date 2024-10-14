015c1660de93ec34eb82b2d03386dd54959d9736
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-10-14 00:28:33 -0300

    feat: Add index.js file with a config object containing all eslint config

---
+++ src/configs/index.js
@@ -0,0 +1,12 @@
+import { defaultConfig } from "./default.js"
+import { formattingConfig } from "./formatting.js"
+import { javascriptConfig } from "./javascript.js"
+import { typescriptConfig } from "./typescript.js"
+
+
+export const configs = {
+  default: defaultConfig,
+  formatting: formattingConfig,
+  javascript: javascriptConfig,
+  typescript: typescriptConfig,
+}
