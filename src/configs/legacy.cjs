f6797b3a382d535f68ea3bd640b449591064835b
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-10-15 19:18:47 -0300

    feat: Remove support from legacy eslint config

---
+++ src/configs/legacy.cjs
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports, no-undef
+const { name } = require("../../package.json")
+
+
+throw new Error(
+  `The Legacy Config is not supported in the current version of ${name}. `
+  + "Please update ESLint to use the Flat Config or verify version compatibility.",
+)
