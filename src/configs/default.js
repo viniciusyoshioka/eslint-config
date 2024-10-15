c025a65c4e683ee48d8b3fd7343f9af443c0b59f
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-10-15 18:36:23 -0300

    feat: Add type comment to default config

--- src/configs/default.js
+++ src/configs/default.js
@@ -3,6 +3,7 @@ import { javascriptConfig } from "./javascript.js"
 import { typescriptConfig } from "./typescript.js"
 
 
+/** @type {import("eslint").Linter.Config[]} */
 export const defaultConfig = [
   ...formattingConfig,
   ...javascriptConfig,
