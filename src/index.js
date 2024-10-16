90218a14c119fdc6ca0110cc923b61e435833ff1
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-10-16 00:00:42 -0300

    feat: Exporting custom globals to library consumer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { defaultConfig } from "./configs/default.js"
 import { formattingConfig } from "./configs/formatting.js"
 import { javascriptConfig } from "./configs/javascript.js"
 import { typescriptConfig } from "./configs/typescript.js"
+import { customGlobals } from "./globals/index.js"
 
 
 export const configs = {
@@ -10,3 +11,5 @@ export const configs = {
   javascript: javascriptConfig,
   typescript: typescriptConfig,
 }
+
+export const globals = customGlobals
