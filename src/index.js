16321e88eca9c700c00a9ebf42bdfe6026af27a0
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-10-15 23:33:53 -0300

    refactor: Move index file that exports the config from `src/config` folder to `src`

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { defaultConfig } from "./configs/default.js"
+import { formattingConfig } from "./configs/formatting.js"
+import { javascriptConfig } from "./configs/javascript.js"
+import { typescriptConfig } from "./configs/typescript.js"
+
+
+export const configs = {
+  default: defaultConfig,
+  formatting: formattingConfig,
+  javascript: javascriptConfig,
+  typescript: typescriptConfig,
+}
