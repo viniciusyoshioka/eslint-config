ceda45a4ab2d512475b2118858ff2320d9ceb35b
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-10-20 20:19:18 -0300

    refactor: Using `typescriptSupportConfig` to add typescript support to javascript config

--- src/configs/javascript.js
+++ src/configs/javascript.js
@@ -1,7 +1,11 @@
 import eslint from "@eslint/js"
-import tslint from "typescript-eslint"
 
-import { alwaysIgnorePaths, jsFileAnyPath, tsFileAnyPath } from "../constants.js"
+import {
+  alwaysIgnorePaths,
+  jsFileAnyPath,
+  tsFileAnyPath,
+  typescriptSupportConfig,
+} from "../constants.js"
 
 
 /** @type {import("eslint").Linter.Config[]} */
@@ -11,18 +15,13 @@ export const javascriptConfig = [
   },
 
   eslint.configs.recommended,
+  typescriptSupportConfig,
   {
     name: "javascript_lint",
     files: [
       ...jsFileAnyPath,
       ...tsFileAnyPath,
     ],
-    plugins: {
-      "@typescript-eslint": tslint.plugin,
-    },
-    languageOptions: {
-      parser: tslint.parser,
-    },
     rules: {
       "no-constant-binary-expression": "warn",
       "no-duplicate-imports": "warn",
