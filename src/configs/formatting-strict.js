75bf1b4046f0762706f55ef966ca80906901ef48
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-10-19 21:13:10 -0300

    feat: Create `formattingStrictConfig`

---
+++ src/configs/formatting-strict.js
@@ -0,0 +1,24 @@
+import { formattingConfig } from "./formatting.js"
+
+
+/** @type {import("eslint").Linter.Config[]} */
+export const formattingStrictConfig = [
+  ...formattingConfig,
+  {
+    name: "typescript_formatting_strict",
+    rules: {
+      "@typescript-eslint/class-literal-property-style": ["warn", "fields"],
+      "@typescript-eslint/no-empty-function": ["error", {
+        allow: [
+          // eslint options
+          "constructors",
+
+          // typescript-eslint options
+          "private-constructors",
+          "protected-constructors",
+          "overrideMethods",
+        ],
+      }],
+    },
+  },
+]
