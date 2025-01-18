8e6e1acc595d773e5a13d4792b83a3bb35a136d6
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2025-01-17 22:55:44 -0300

    remove: Delete strict config variants

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import { formattingStrictConfig } from "./configs/formatting-strict.js"
 import { formattingConfig } from "./configs/formatting.js"
 import { javascriptConfig } from "./configs/javascript.js"
-import { recommendedStrictConfig } from "./configs/recommended-strict.js"
 import { recommendedConfig } from "./configs/recommended.js"
 import { typescriptConfig } from "./configs/typescript.js"
 
 
 export const configs = {
   recommended: recommendedConfig,
-  recommendedStrict: recommendedStrictConfig,
   formatting: formattingConfig,
-  formattingStrict: formattingStrictConfig,
   javascript: javascriptConfig,
   typescript: typescriptConfig,
 }
