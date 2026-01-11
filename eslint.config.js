9cffba805e9a02e9ec6c0cf78b718109fe334c8e
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-01-11 15:39:12 -0300

    chore: Remove 'tsconfig.build.json' to simplify config

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,18 @@ import { configs } from './src/index.js'
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   ...configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        projectService: {
+          allowDefaultProject: [
+            'tests/formatting/javascript.js',
+            'tests/formatting/typescript.ts',
+            'eslint.config.js',
+          ],
+        },
+      },
+    },
+  },
 ]
